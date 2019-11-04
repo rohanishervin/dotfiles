e9969615d2311436c6696dc80af3de69d3bc2ddf
Shervin Rohani <shervin.rohani72@gmail.com>
2019-11-04 21:27:25 +0330

    update: kitty config added

--- install.sh
+++ install.sh
@@ -10,3 +10,11 @@ ln -sf "$PWD/nvim" "$HOME/.config/nvim"
 # install vimplug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# tint2
+mkdir -p "$HOME/.config/tint2/"
+ln -sf $PWD/tint2/tint2rc $HOME/.config/tint2/tint2rc
+
+# kitty
+rm -rf "$HOME/.config/kitty/kitty.conf"
+ln -sf "$PWD/kitty/kitty.conf" "$HOME/.config/kitty/kitty.conf"
